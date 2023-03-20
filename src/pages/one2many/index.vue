89057a9e891e34676ac8c80bd1ba48228da23742
iswinter1993 <iswinter@aliyun.com>
2023-03-20 16:05:31 +0800

    弹幕功能

--- src/pages/one2many/index.vue
+++ src/pages/one2many/index.vue
@@ -84,12 +84,12 @@
         <el-row>
           <div style="width: 50%; display: flex; flex-direction: row; height: auto">
             <el-input v-model="state.message"></el-input>
-            <!-- <el-button @click="sendMsgToPub()">发送弹幕</el-button> -->
+            <el-button @click="sendMsgToPub()">发送弹幕</el-button>
           </div>
         </el-row>
         <el-row>
-            <img :src="logo" class="pre" />
-            <el-button @click="onSelectImage({src:logo})">改变背景</el-button>
+          <img :src="logo" class="pre" />
+          <el-button @click="onSelectImage({ src: logo })">改变背景</el-button>
         </el-row>
       </el-col>
     </el-row>
@@ -97,7 +97,7 @@
 </template>
 
 <script setup lang="ts">
-import logo from '@/assets/1.jpg'
+import logo from "@/assets/1.jpg";
 import { reactive } from "vue";
 import { useDeviceStore } from "../../store/useDeviceStore";
 import { getParams, handleError } from "../demo2/shareMedia";
@@ -108,6 +108,8 @@ import { getLocalUserMedia } from "../demo1/getMedia";
 import * as SFS from "@mediapipe/selfie_segmentation";
 //FPS计算工具
 import FPSC from "@mediapipe/control_utils";
+//弹幕组件
+import Danmaku from "danmaku";
 const PeerConnection =
   window.RTCPeerConnection ||
   window.mozRTCPeerConnection ||
@@ -117,7 +119,7 @@ const state: any = reactive({
   canvasElement: null,
   canvasCtx: null,
   image: null,
-//   selfieSegmentation: null,
+  //   selfieSegmentation: null,
   formInline: {
     rtcmessage: undefined,
     rtcmessageRes: undefined, //响应
@@ -153,7 +155,8 @@ const state: any = reactive({
   dataChannelMap: new Map(),
   fpsControl: null,
   virtualMediaStream: null,
-  rfId:null
+  rfId: null,
+  danmaku: undefined, //弹幕组件
 });
 
 state.formInline.nickname = getParams("nickname");
@@ -174,7 +177,7 @@ const init = () => {
     setTimeout(async () => {
       if (state.roomUserList.length) {
         await initMeetingRoomPc();
-        // initDanmuContainer()
+        initDanmuContainer();
       }
     }, 2000);
   });
@@ -215,96 +218,172 @@ const init = () => {
       onCandiDate(e["data"]["userId"], e["data"]["candidate"]);
     }
   });
-  const onRemoteOffer = async (fromUid: any, offer: any) => {
-    const localUid = state.formInline.userId;
-    let pcKey = localUid + "-" + fromUid;
-    let pc = new PeerConnection(state.rtcPcParams);
-    state.RtcPcMaps.set(pcKey, pc);
-    console.log("主播监听到远端offer");
-    onPcEvent(pc, localUid, fromUid);
-    //NOTE: 主播端创建数据通道
-    await createDataChannel(pc, localUid, fromUid);
-    //正常背景时
-    if(!state.virtualMediaStream){
-        for (const track of state.localStream.getTracks()) {
-          pc.addTrack(track);
-        }
-    }else{
-        //设置虚拟背景时
-        //注意这里我们直接用的是虚拟背景流 virtualMediaStream 替换原先的 localstream
-        for (const track of state.virtualMediaStream.getTracks()) {
-          pc.addTrack(track);
-        }
-    }
-    pc.setRemoteDescription(offer);
-    let answer = await pc.createAnswer();
-    await pc.setLocalDescription(answer);
-    let params = { targetUid: fromUid, userId: localUid, answer: answer };
-    state.linkSocket.emit("answer", params);
-  };
-  const onRemoteAnswer = async (fromUid: any, answer: any) => {
-    const localUid = state.formInline.userId;
-    let pcKey = localUid + "-" + fromUid;
-    let pc = state.RtcPcMaps.get(pcKey);
-    await pc.setRemoteDescription(answer);
-  };
-  const onCall = (e: any) => {
-    console.log(e);
-  };
-  const onCandiDate = (fromUid: any, candidate: any) => {
-    const localUid = state.formInline.userId;
-    let pcKey = localUid + "-" + fromUid;
-    let pc = state.RtcPcMaps.get(pcKey);
-    pc.addIceCandidate(candidate);
-  };
-  const initMeetingRoomPc = async () => {
-    //是否是主播
-    if (state.formInline.pub === "pub") {
-      state.localStream = await getLocalUserMedia({ audio: false, video: true });
-      await setDomVideoStream("localdemo01", state.localStream);
-    }
-    const localUid = state.formInline.userId;
-    //找到当前房间的视频流发布者 即主播
-    let publisher = state.roomUserList
-      .filter((e: any) => e.userId !== localUid && e.pub === "pub")
-      .map((e: any, index: any) => {
-        return e.userId;
-      });
-    if (publisher.length > 0) {
-      console.log("找到当前房间的视频流发布者:", publisher);
-      publisher = publisher[0];
-      state.publisher = publisher;
-    } else {
-      console.log("无视频发布者");
-      return;
-    }
-    console.log("观众维护与主播的pc");
-    //和发布者建立RTC连接 不发送自己视频流
-    //学生维护与老师的pc
-    let pcKey = localUid + "-" + publisher;
-    console.log("pcKey", pcKey);
-    let pc = state.RtcPcMaps.get(pcKey);
-    if (!pc) {
-      pc = new PeerConnection(state.rtcPcParams);
-      state.RtcPcMaps.set(pcKey, pc);
-    }
-    // sendrecv 既发送也接受对方媒体 sendonly 仅发送不接受 recvonly 仅接受 不发送 如何不设置 则不发送也不接受
-    pc.addTransceiver("audio", { direction: "recvonly" });
-    pc.addTransceiver("video", { direction: "recvonly" });
-    onPcEvent(pc, localUid, publisher);
-    //创建数据通道
-    await createDataChannel(pc, localUid, publisher);
-    //创建offer
-    let offer = await pc.createOffer();
-    //设置offer未本地描述
-    await pc.setLocalDescription(offer);
-    //发送offer给被呼叫端
-    let params = { targetUid: publisher, userId: localUid, offer: offer };
-    state.linkSocket.emit("offer", params);
-  };
+
   state.linkSocket.on("error", (e: any) => {
     console.log("error", e);
   });
+};
+const onRemoteOffer = async (fromUid: any, offer: any) => {
+  const localUid = state.formInline.userId;
+  let pcKey = localUid + "-" + fromUid;
+  let pc = new PeerConnection(state.rtcPcParams);
+  state.RtcPcMaps.set(pcKey, pc);
+  console.log("主播监听到远端offer");
+  onPcEvent(pc, localUid, fromUid);
+  //NOTE: 主播端创建数据通道
+  await createDataChannel(pc, localUid, fromUid);
+  //正常背景时
+  if (!state.virtualMediaStream) {
+    for (const track of state.localStream.getTracks()) {
+      pc.addTrack(track);
+    }
+  } else {
+    //设置虚拟背景时
+    //注意这里我们直接用的是虚拟背景流 virtualMediaStream 替换原先的 localstream
+    for (const track of state.virtualMediaStream.getTracks()) {
+      pc.addTrack(track);
+    }
+  }
+  pc.setRemoteDescription(offer);
+  let answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+  let params = { targetUid: fromUid, userId: localUid, answer: answer };
+  state.linkSocket.emit("answer", params);
+};
+const onRemoteAnswer = async (fromUid: any, answer: any) => {
+  const localUid = state.formInline.userId;
+  let pcKey = localUid + "-" + fromUid;
+  let pc = state.RtcPcMaps.get(pcKey);
+  await pc.setRemoteDescription(answer);
+};
+const onCall = (e: any) => {
+  console.log(e);
+};
+const onCandiDate = (fromUid: any, candidate: any) => {
+  const localUid = state.formInline.userId;
+  let pcKey = localUid + "-" + fromUid;
+  let pc = state.RtcPcMaps.get(pcKey);
+  pc.addIceCandidate(candidate);
+};
+const initMeetingRoomPc = async () => {
+  //是否是主播
+  if (state.formInline.pub === "pub") {
+    state.localStream = await getLocalUserMedia({ audio: false, video: true });
+    await setDomVideoStream("localdemo01", state.localStream);
+  }
+  const localUid = state.formInline.userId;
+  //找到当前房间的视频流发布者 即主播
+  let publisher = state.roomUserList
+    .filter((e: any) => e.userId !== localUid && e.pub === "pub")
+    .map((e: any, index: any) => {
+      return e.userId;
+    });
+  if (publisher.length > 0) {
+    console.log("找到当前房间的视频流发布者:", publisher);
+    publisher = publisher[0];
+    state.publisher = publisher;
+  } else {
+    console.log("无视频发布者");
+    return;
+  }
+  console.log("观众维护与主播的pc");
+  //和发布者建立RTC连接 不发送自己视频流
+  //学生维护与老师的pc
+  let pcKey = localUid + "-" + publisher;
+  console.log("pcKey", pcKey);
+  let pc = state.RtcPcMaps.get(pcKey);
+  if (!pc) {
+    pc = new PeerConnection(state.rtcPcParams);
+    state.RtcPcMaps.set(pcKey, pc);
+  }
+  // sendrecv 既发送也接受对方媒体 sendonly 仅发送不接受 recvonly 仅接受 不发送 如何不设置 则不发送也不接受
+  pc.addTransceiver("audio", { direction: "recvonly" });
+  pc.addTransceiver("video", { direction: "recvonly" });
+  onPcEvent(pc, localUid, publisher);
+  //1.初始化 RTCPeerConnection => initPc
+  //2.初始化基础监听 initPc.ontrack() initPc.ondatachannel() initPc.onicecandidate()
+  //3.执行创建数据通道
+  await createDataChannel(pc, localUid, publisher);
+  //创建offer
+  let offer = await pc.createOffer();
+  //设置offer未本地描述
+  await pc.setLocalDescription(offer);
+  //发送offer给被呼叫端
+  let params = { targetUid: publisher, userId: localUid, offer: offer };
+  state.linkSocket.emit("offer", params);
+};
+/**
+ * 初始化弹幕容器
+ */
+const initDanmuContainer = () => {
+  if (state.formInline.pub === "pub") {
+    //主播
+    //增加弹幕组件
+    state.danmaku = new Danmaku({
+      container: document.getElementById("localdemo01Parent"),
+      speed: 30,
+    });
+  } else {
+    //客户端
+    state.danmaku = new Danmaku({
+      container: document.getElementById("publisherVideoParent"),
+      speed: 30,
+    });
+  }
+  //首条弹幕
+  state.danmaku.emit({
+    text: "直播间已开启，请踊跃发言",
+    style: { fontSize: "20px", color: "#ff5500" },
+  });
+};
+/**
+ * 指定数据通道发送数据
+ * @param {Object} uid
+ * @param {Object} remoteId
+ * @param {Object} msg
+ */
+const clientDataChannelMsg = (uid: any, remoteId: any, msg: any) => {
+  let c = state.dataChannelMap.get(uid + "-" + remoteId);
+  if (c) {
+    c.send(msg);
+  }
+};
+/**
+ * 发布弹幕
+ */
+const sendMsgToPub = (msg = undefined) => {
+  if (!msg) {
+    msg = state.message;
+  }
+  //如果是主播 则遍历所有数据通道给每个客户端发送消息
+  if (state.formInline.pub === "pub") {
+    state.dataChannelMap.forEach((index: any, k: any) => {
+      state.dataChannelMap.get(k).send(msg);
+    });
+    danmuForRoller(msg); //上屏幕
+  } else {
+    //私信给主播 主播收到再广播（所以无需自己上屏幕）
+    clientDataChannelMsg(state.formInline.userId, state.publisher, msg);
+  }
+  state.message = undefined;
+};
+const onAllMessage = (msg: any) => {
+  danmuForRoller(msg); //收到消息上屏幕
+  //主播收到客户端的消息 广播
+  if (state.formInline.pub === "pub") {
+    state.dataChannelMap.forEach((index: any, k: any) => {
+      state.dataChannelMap.get(k).send(msg);
+    });
+  }
+};
+/**
+ * 直播留言弹幕
+ * @param {Object} msg
+ */
+const danmuForRoller = (msg: any) => {
+  if (state.danmaku) {
+    state.danmaku.emit({ text: msg, style: { fontSize: "20px", color: "#ff5500" } });
+  }
 };
 const onPcEvent = (pc: any, localUid: any, remoteUid: any) => {
   pc.ontrack = function (event: any) {
@@ -320,7 +399,7 @@ const onPcEvent = (pc: any, localUid: any, remoteUid: any) => {
     ev.channel.onmessage = function (data: any) {
       console.log("用户：" + remoteUid + " 数据通道消息", data.data);
       // 弹幕上屏幕
-      // that.onAllMessage(data.data)
+        onAllMessage(data.data)
     };
     ev.channel.onclose = function () {
       console.log("用户：" + remoteUid + " 数据通道关闭");
@@ -371,7 +450,7 @@ const setDomVideoTrick = (domId: any, trick: any) => {
     video.muted = true;
   }
 };
-let selfieSegmentation:any;
+let selfieSegmentation: any;
 const setDomVideoStream = async (domId: any, newStream: any) => {
   let video: any = document.getElementById(domId);
   console.log("渲染本地视频", video, newStream);
@@ -393,7 +472,7 @@ const onSelectImage = async (e: any) => {
 };
 //重新初始化分割模型并获取新的流
 const changeBg = async (src: any) => {
-console.log('改变虚拟背景')
+  console.log("改变虚拟背景");
   await initVb(src);
   //虚拟背景流
   //虚拟背景流暂存变量
@@ -405,21 +484,21 @@ console.log('改变虚拟背景')
 /**
  * 初始化虚拟背景
  */
-const initVb =async (src: any) => {
-    console.log('初始化虚拟背景')
+const initVb = async (src: any) => {
+  console.log("初始化虚拟背景");
   state.canvasElement = document.getElementById("output_canvas");
   state.canvasCtx = state.canvasElement.getContext("2d");
   state.image = new Image();
   state.image.src = src;
-  console.log('下载模型')
-selfieSegmentation =await new SFS.SelfieSegmentation({
+  console.log("下载模型");
+  selfieSegmentation = await new SFS.SelfieSegmentation({
     locateFile: (file) => {
       console.log(file);
-    //   return `http://127.0.0.1:8081/${file}`; //ng  代理模型文件夹
+      //   return `http://127.0.0.1:8081/${file}`; //ng  代理模型文件夹
       return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1.1632777926/${file}`;
     },
   });
-  console.log('训练模型')
+  console.log("训练模型");
   await selfieSegmentation.setOptions({
     staticImageModel: false,
     maxNumHands: 2,
@@ -452,52 +531,72 @@ const changeRemoteStream = async (stream: any) => {
 };
 /**
  * 图像分割后处理背景和人像
- * 
+ *
  **/
-const handleResults = (results:any) => {
-    //统计FPS
-    state.fpsControl.tick() 
-    // Prepare the nthis.fpsControl ew frame
-    state.canvasCtx.save();
-    state.canvasCtx.clearRect(0, 0, state.canvasElement.width, state.canvasElement.height);
-    state.canvasCtx.drawImage(results.segmentationMask, 0, 0, state.canvasElement.width, state.canvasElement.height);
-    // Draw the image as the new background, and the segmented video on top of that
-    //利用canvas绘制新背景 
-    console.log('利用canvas绘制新背景')
-   //canvasCtx.globalCompositeOperation = 'source-in';则意味着处理分割后图像中的人体。 
-    state.canvasCtx.globalCompositeOperation = 'source-out';
-    state.canvasCtx.drawImage(state.image, 0, 0, state.image.width, state.image.height, 0, 0, state.canvasElement.width, state.canvasElement.height);
-    state.canvasCtx.globalCompositeOperation = 'destination-atop';
-    state.canvasCtx.drawImage(results.image, 0, 0, state.canvasElement.width, state.canvasElement.height);
-    // Done
-    state.canvasCtx.restore();
-    
-}
+const handleResults = (results: any) => {
+  //统计FPS
+  state.fpsControl.tick();
+  // Prepare the nthis.fpsControl ew frame
+  state.canvasCtx.save();
+  state.canvasCtx.clearRect(0, 0, state.canvasElement.width, state.canvasElement.height);
+  state.canvasCtx.drawImage(
+    results.segmentationMask,
+    0,
+    0,
+    state.canvasElement.width,
+    state.canvasElement.height
+  );
+  // Draw the image as the new background, and the segmented video on top of that
+  //利用canvas绘制新背景
+  console.log("利用canvas绘制新背景");
+  //canvasCtx.globalCompositeOperation = 'source-in';则意味着处理分割后图像中的人体。
+  state.canvasCtx.globalCompositeOperation = "source-out";
+  state.canvasCtx.drawImage(
+    state.image,
+    0,
+    0,
+    state.image.width,
+    state.image.height,
+    0,
+    0,
+    state.canvasElement.width,
+    state.canvasElement.height
+  );
+  state.canvasCtx.globalCompositeOperation = "destination-atop";
+  state.canvasCtx.drawImage(
+    results.image,
+    0,
+    0,
+    state.canvasElement.width,
+    state.canvasElement.height
+  );
+  // Done
+  state.canvasCtx.restore();
+};
 /**
  * 监听触发模型处理
  * Canvas 本身只是一个画布，但是有对应的 API，可以将画布上的每一帧捕捉并形成媒体流.
  * canvasElement.captureStream方法，通过此方法即可捕捉画布并转换成流。内部唯一的参数就是帧速率FPS，一般设置为 20 到 25 这个区间即可满足正常视觉上的视频流畅度
  */
 const virtualBg = async () => {
-    let video:any = document.getElementById('localdemo01')
-    if(state.rfId){
-        cancelAnimationFrame(state.rfId)
+  let video: any = document.getElementById("localdemo01");
+  if (state.rfId) {
+    cancelAnimationFrame(state.rfId);
+  }
+  let lastTime = performance.now();
+  async function getFrames() {
+    const now = video.currentTime;
+    //高FPS 尝试将下面的判断注释
+    if (now > lastTime) {
+      await selfieSegmentation.send({ image: video });
     }
-    let lastTime = performance.now();
-    async function getFrames() {
-        const now = video.currentTime;
-        //高FPS 尝试将下面的判断注释
-        if(now > lastTime){
-            await selfieSegmentation.send({image: video});
-        }
-        lastTime = now;
-        //无限定时循环 退出记得取消 cancelAnimationFrame() 
-        state.rfId = requestAnimationFrame(getFrames);
-        
-    };
-    getFrames()
-    return state.canvasElement.captureStream(25)
-}
+    lastTime = now;
+    //无限定时循环 退出记得取消 cancelAnimationFrame()
+    state.rfId = requestAnimationFrame(getFrames);
+  }
+  getFrames();
+  return state.canvasElement.captureStream(25);
+};
 if (state.formInline.nickname && state.formInline.roomId && state.formInline.userId) {
   init();
 }
@@ -509,58 +608,58 @@ if (state.formInline.nickname && state.formInline.roomId && state.formInline.use
   height: 400px;
   width: 400px;
 }
-html,body{
-		margin: 0;
-		padding: 0;
-	}
-	
-	.container{
-		margin: 20px 0 20px 0;
-		padding: 0;
-		width: 98%;
-		height: 90vh;
-	}
-	/* 所有流 */
-	#allVideo{
-		padding: 5px;
-		height: auto;
-		/* position: relative;
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  margin: 20px 0 20px 0;
+  padding: 0;
+  width: 98%;
+  height: 90vh;
+}
+/* 所有流 */
+#allVideo {
+  padding: 5px;
+  height: auto;
+  /* position: relative;
 		display: flex;flex-direction: row;justify-content: flex-start;flex-wrap: wrap; */
-	}
-	/* 主播端浮窗 */
-	#localDomId{
-		/* border: 1px red solid; */
-		display: flex;
-		flex-direction: row;
-		
-	}
-	
-	.frame-videos{
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		justify-content: flex-start;
-		padding: 30px;
-	}
-	.frame-videos div{
-		border: 1px blueviolet solid;
-		width: 23%;
-		height: 200px;
-		position: relative;
-		background-color: black;
-	}
-	.frame-videos div label{
-		color: white;
-		position: absolute;
-		bottom: 2px;
-		left: 2px;
-	}
-	.dialog-inner-container{
-		margin-left:35%;
-		margin-top: 5%;
-	}
-    .pre{
-        width: 250px;
-        height: 200px;
-    }
+}
+/* 主播端浮窗 */
+#localDomId {
+  /* border: 1px red solid; */
+  display: flex;
+  flex-direction: row;
+}
+
+.frame-videos {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding: 30px;
+}
+.frame-videos div {
+  border: 1px blueviolet solid;
+  width: 23%;
+  height: 200px;
+  position: relative;
+  background-color: black;
+}
+.frame-videos div label {
+  color: white;
+  position: absolute;
+  bottom: 2px;
+  left: 2px;
+}
+.dialog-inner-container {
+  margin-left: 35%;
+  margin-top: 5%;
+}
+.pre {
+  width: 250px;
+  height: 200px;
+}
 </style>
